fad422fd02c65ad5537f51d19c761d9fb8237e92
Isaac Hardy <isaac@isaachardy.io>
2018-07-01 16:26:44 -0400

    added logos, images, and colors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,13 @@
 import React from 'react';
 // import Link from 'gatsby-link';
 
-import logo from '../assets/images/logo.svg';
+import logo from '../assets/images/logo1.png';
 
 const Header = () => (
   <header id="header" className="alt">
     <span className="logo">
       <img src={logo} alt="" />
     </span>
-    <h1>Stellar</h1>
-    <p>
-      Just another free, fully responsive site template<br />
-      design by <a href="https://html5up.net">HTML5 UP</a>.
-    </p>
   </header>
 );
 
